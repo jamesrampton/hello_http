b99c46aaf5db71180b988fe3ab5128bf42accaaf
James Rampton <me@jamesrampton.com>
2023-12-10 09:40:52 +0000

    well, hyper has changed a lot since the article was written!

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,44 @@
 #[tokio::main]
-async fn main() {
-    let response = reqwest::get("https://example.org").await.unwrap();
-    println!(
-        "Got HTTP {}, with headers: {:#?}",
-        response.status(),
-        response.headers()
-    );
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let url = "http://example.org".parse::<hyper::Uri>()?;
+    let host = url.host().expect("url to have a host");
+    let port = url.port_u16().unwrap_or(80);
+    let address = format!("{host}:{port}");
+    let stream = TcpStream::connect(address).await?;
+    let io = TokioIo::new(stream);
+    let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
 
-    let body = response.text().await.unwrap();
+    tokio::task::spawn(async move {
+        if let Err(err) = conn.await {
+            println!("Connection failed: {:?}", err);
+        }
+    });
 
-    let num_lines = 10;
-    println!("First {num_lines} lines of body:");
-    for line in body.lines().take(num_lines) {
-        println!("{line}");
+    let authority = url.authority().unwrap().clone();
+
+    let req = Request::builder()
+        .uri(url)
+        .header(hyper::header::HOST, authority.as_str())
+        .body(Empty::<Bytes>::new())?;
+
+    let mut res = sender.send_request(req).await?;
+
+    println!("Response status: {}", res.status());
+
+    while let Some(next) = res.frame().await {
+        let frame = next?;
+        if let Some(chunk) = frame.data_ref() {
+            stdout().write_all(chunk).await?;
+        }
     }
+    Ok(())
 }
+
+use http_body_util::BodyExt;
+use http_body_util::Empty;
+use hyper::body::Bytes;
+use hyper::Request;
+use hyper_util::rt::TokioIo;
+use tokio::io::stdout;
+use tokio::io::AsyncWriteExt as _;
+use tokio::net::TcpStream;
