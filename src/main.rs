e6400bd2d18b2fe3efb77c0405f04335cca4adb4
James Rampton <me@jamesrampton.com>
2023-12-10 09:44:05 +0000

    make the output more like what I'm following in the article

--- src/main.rs
+++ src/main.rs
@@ -21,24 +21,32 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         .header(hyper::header::HOST, authority.as_str())
         .body(Empty::<Bytes>::new())?;
 
-    let mut res = sender.send_request(req).await?;
+    let mut response = sender.send_request(req).await?;
 
-    println!("Response status: {}", res.status());
+    println!(
+        "Got HTTP {}, with headers: {:#?}",
+        response.status(),
+        response.headers()
+    );
 
-    while let Some(next) = res.frame().await {
-        let frame = next?;
-        if let Some(chunk) = frame.data_ref() {
-            stdout().write_all(chunk).await?;
-        }
-    }
+    let body = response.body();
+
+    println!("Body: {:?}", body);
+
+    // while let Some(next) = response.frame().await {
+    //     let frame = next?;
+    //     if let Some(chunk) = frame.data_ref() {
+    //         stdout().write_all(chunk).await?;
+    //     }
+    // }
     Ok(())
 }
 
-use http_body_util::BodyExt;
+// use http_body_util::BodyExt;
 use http_body_util::Empty;
 use hyper::body::Bytes;
 use hyper::Request;
 use hyper_util::rt::TokioIo;
-use tokio::io::stdout;
-use tokio::io::AsyncWriteExt as _;
+// use tokio::io::stdout;
+// use tokio::io::AsyncWriteExt as _;
 use tokio::net::TcpStream;
